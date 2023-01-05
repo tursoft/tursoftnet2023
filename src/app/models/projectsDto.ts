569805a883c390e706b5a075427711a2a1985f9b
Muhammet Turşak <tursoft@gmail.com>
2023-01-05 06:15:35 +0300

    projects section initial implementation

--- src/app/models/projectsDto.ts
+++ src/app/models/projectsDto.ts
@@ -1,3 +1,46 @@
+export interface ProjectItemDto {
+  id: number,
+  name: string,
+  title?: string,
+  group?: string,
+  company?: string,
+  value?: number,
+  icon?: string,
+  summary?: string,
+  fulltext?: string | string[],
+
+  datePeriod?: { 
+    startDate: string,
+    endDate?: string | null
+  },
+  
+  props?: { 
+      name:string,
+      title: string
+  }[],
+
+  domains?: {
+    name: string, 
+    title: string,
+    value: number,
+    iconCss?: string
+  }[],
+  
+  team?: { 
+      position?: string,
+      name?: string,
+      contactNo?: string
+  }[],
+
+  modules?: string[],
+  customers?: string[],
+  partners?: string[],
+
+  technologies?: {
+    name:  string,
+    type: string
+  }[]
+}
 export interface ProjectsDto {
     general: {
         title: string,
@@ -10,48 +53,5 @@ export interface ProjectsDto {
         }[]
       },
       
-    items: {
-      id: number,
-      name: string,
-      title?: string,
-      group?: string,
-      company?: string,
-      value?: number,
-      icon?: string,
-      summary?: string,
-      fulltext?: string | string[],
-
-      datePeriod?: { 
-        startDate: string,
-        endDate?: string | null
-      },
-      
-      props?: { 
-          name:string,
-          title: string
-      }[],
-
-      domains?: {
-        name: string, 
-        title: string,
-        value: number,
-        iconCss?: string
-      }[],
-      
-      team?: { 
-          position?: string,
-          name?: string,
-          contactNo?: string
-      }[],
-
-      modules?: string[],
-      customers?: string[],
-      partners?: string[],
-
-      technologies?: {
-        name:  string,
-        type: string
-      }[]
-
-    }[]
+    items: ProjectItemDto[]
 }
--- src/app/pages/home/sections/projects/projects.component.ts
+++ src/app/pages/home/sections/projects/projects.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ProjectsDto } from 'src/app/models/projectsDto';
+import { ProjectItemDto, ProjectsDto } from 'src/app/models/projectsDto';
 import projectsData from '../../../../data/projects.json';
 
 @Component({
@@ -7,6 +7,31 @@ import projectsData from '../../../../data/projects.json';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+export class ProjectsComponent 
+{
   data: ProjectsDto = projectsData;
+  filterText: string = '';
+  filteredItems: ProjectItemDto[] = [];
+
+  constructor() {
+    this.filterItems();
+  }
+
+  filterItems = () => {
+    const { filterText, data: { items }} = this;
+    let filteredItems: ProjectItemDto[] = items ?? [];
+
+    if ((filterText ?? '').trim() != '') {
+      filteredItems = filteredItems.filter(i => 
+                                    (i.name ?? '').includes(filterText) ||
+                                    (i.name ?? '').toLowerCase().includes(filterText.toLowerCase()) ||
+                                    (i.title ?? '').includes(filterText) ||
+                                    (i.title ?? '').toLowerCase().includes(filterText.toLowerCase()) ||
+                                    (i.company ?? '').includes(filterText) ||
+                                    (i.company ?? '').toLowerCase().includes(filterText.toLowerCase())
+                                  );
+    }
+    console.log('filteredItems:', { filterText, items, filteredItems });
+    this.filteredItems = filteredItems;
+  }
 }
