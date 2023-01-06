b75a70aa694567eb4395defc95244474a25253f5
Muhammet Turşak <tursoft@gmail.com>
2023-01-07 00:59:12 +0300

    experiences section started

--- src/app/pages/home/sections/experiences/experiences.component.ts
+++ src/app/pages/home/sections/experiences/experiences.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { from } from 'linq-to-typescript';
+import { TimelineItem } from 'src/app/components/verticaltimeline/verticaltimeline.component';
+import experiencesData from '../../../../data/experiences.json';
 
 @Component({
   selector: 'app-experiences',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./experiences.component.scss']
 })
 export class ExperiencesComponent {
-
+  items: TimelineItem[] = [];
+  
+  constructor() {
+    this.items = from(experiencesData.items)
+                .orderBy(e => e.orderIndex)
+                .select(e => <TimelineItem>{
+                  code: e.id.toString(),
+                  title1: e.companyName,
+                  title2: e.positions[0].startDate + ' - ' + (e.positions[e.positions.length-1].endDate ?? 'Present'),
+                  content: `
+                  positions: ${e.positions}<br/>
+                  icon: ${e.icon}<br/>
+                  `
+                })
+                .toArray();
+  }
 }
