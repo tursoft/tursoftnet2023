faedac9653b64474fa96ac654af176977b2cb367
Muhammet Turşak <tursoft@gmail.com>
2023-01-07 00:58:09 +0300

    projects section implemented

--- src/app/pages/home/sections/projects/projects.component.ts
+++ src/app/pages/home/sections/projects/projects.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
-import { ProjectItemDto, ProjectsDto } from 'src/app/models/projectsDto';
+import { from } from 'linq-to-typescript';
+import { ProjectGroupDto, ProjectItemDto, ProjectsDto } from 'src/app/models/projectsDto';
 import projectsData from '../../../../data/projects.json';
 
+interface FilterDto {
+  filterText?: string,
+  group?: string
+}
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -10,18 +15,24 @@ import projectsData from '../../../../data/projects.json';
 export class ProjectsComponent 
 {
   data: ProjectsDto = projectsData;
-  filterText: string = '';
+  groups: ProjectGroupDto[] = [];
   filteredItems: ProjectItemDto[] = [];
+  filter: FilterDto = {};
 
   constructor() {
+    const groupAll: ProjectGroupDto = {
+      name: undefined,
+      title: 'All'
+    };
+    this.groups = [groupAll, ...this.data.general.groups];
     this.filterItems();
   }
 
   filterItems = () => {
-    const { filterText, data: { items }} = this;
+    const { filter, filter: { filterText }, data: { items }} = this;
     let filteredItems: ProjectItemDto[] = items ?? [];
 
-    if ((filterText ?? '').trim() != '') {
+    if (filterText && filterText.trim() != '') {
       filteredItems = filteredItems.filter(i => 
                                     (i.name ?? '').includes(filterText) ||
                                     (i.name ?? '').toLowerCase().includes(filterText.toLowerCase()) ||
@@ -31,7 +42,22 @@ export class ProjectsComponent
                                     (i.company ?? '').toLowerCase().includes(filterText.toLowerCase())
                                   );
     }
-    console.log('filteredItems:', { filterText, items, filteredItems });
+    
+    if ((filter.group ?? '') != '') {
+      filteredItems = filteredItems.filter(i => i.group == filter.group);
+    }
+    
     this.filteredItems = filteredItems;
+  }
+
+  setGroup(group: string | undefined) {
+    this.filter.group = group;
+    this.filterItems();
+  }
+
+  clearFilter() {
+    this.filter.filterText = undefined;
+    this.filter.group = undefined;
+    this.filterItems();
   }
 }
