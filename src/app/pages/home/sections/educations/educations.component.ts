b1d678463d682cbc0826d214bf9d6977df470607
Muhammet Turşak <tursoft@gmail.com>
2023-01-07 00:59:01 +0300

    educations section implemented

--- src/app/pages/home/sections/educations/educations.component.ts
+++ src/app/pages/home/sections/educations/educations.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TimelineItem } from 'src/app/components/verticaltimeline/verticaltimeline.component';
+import educationData from '../../../../data/education.json';
 
 @Component({
   selector: 'app-educations',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./educations.component.scss']
 })
 export class EducationsComponent {
+  items: TimelineItem[] = [];
 
+  constructor() {
+    this.items = educationData.items.map(e => <TimelineItem>{
+      code: e.id.toString(),
+      title1: e.level,
+      title2: e.department,
+      content: `
+      <div class='timeline-body'>
+        <div class='col1'>
+          <strong>${e.name}</strong><br/>
+          ${e.city} / ${e.datePeriod.startDate + ' - ' + (e.datePeriod.endDate ?? 'Present')}<br/>
+          Graduation Date: ${e.graduateDate}<br/>
+        </div>
+
+        <div class='col2'>
+          <img class='box-icon' src='/assets/images/academic/${e.icon}'>
+        </div>        
+      </div>
+      `
+    });
+  }
 }
