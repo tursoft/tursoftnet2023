b40d41549259a826afb7991b1373f6d313501b13
Muhammet Turşak <tursoft@gmail.com>
2023-01-07 23:09:34 +0300

    skills section implemented

--- src/app/pages/home/sections/skills/skills.component.ts
+++ src/app/pages/home/sections/skills/skills.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { from } from 'linq-to-typescript';
+import { SkillGroupDto, SkillItemDto, SkillsDto } from 'src/app/models/skillsDto';
+import skillsData from '../../../../data/skills.json';
 
+interface FilterDto {
+  filterText?: string,
+  group?: string
+}
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
+  data: SkillsDto = skillsData;
+  
+  groups: SkillGroupDto[] = [];
+  filteredItems: SkillItemDto[] = [];
+  filter: FilterDto = {};
 
+  constructor() {
+    const groupAll: SkillGroupDto = {
+      name: undefined,
+      title: 'All'
+    };
+    this.groups = [groupAll, ...this.data.general.groups];
+    this.filterItems();
+  }
+
+  qetItemIconName(item: SkillItemDto) {
+    return item.name.replaceAll(' ','')
+                    .replace('.', '')
+                    .replace('#', 'sharp')
+                    .toLowerCase() + '.png';
+  }
+
+  filterItems = () => {
+    const { filter, filter: { filterText }, data: { items }} = this;
+    let filteredItems: SkillItemDto[] = items ?? [];
+
+    if (filterText && filterText.trim() != '') {
+      filteredItems = filteredItems.filter(i => 
+                                    (i.name ?? '').includes(filterText) ||
+                                    (i.name ?? '').toLowerCase().includes(filterText.toLowerCase()) ||
+                                    (i.title ?? '').includes(filterText) ||
+                                    (i.title ?? '').toLowerCase().includes(filterText.toLowerCase())
+                                  );
+    }
+    
+    if ((filter.group ?? '') != '') {
+      filteredItems = filteredItems.filter(i => i.group == filter.group);
+    }
+    
+    this.filteredItems = filteredItems;
+  }
+
+  setGroup(group: string | undefined) {
+    this.filter.group = group;
+    this.filterItems();
+  }
+
+  clearFilter() {
+    this.filter.filterText = undefined;
+    this.filter.group = undefined;
+    this.filterItems();
+  }
 }
