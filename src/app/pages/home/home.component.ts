9e0df16d3e93b9ca3fb29915ef39f1e77806f02e
Muhammet Turşak <tursoft@gmail.com>
2023-01-08 22:12:43 +0300

    set top menu visibility for home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Session } from 'src/app/services/session';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  constructor(public session: Session) {
+    this.session.showTopMenu=true;
+  }
 }
