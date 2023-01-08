d72ebc3f6fbd662ea427de8f113f05fa06168192
Muhammet Turşak <tursoft@gmail.com>
2023-01-08 22:13:22 +0300

    project details page added

--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { ProjectsDto } from 'src/app/models/projectsDto';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectItemDto, ProjectsDto } from 'src/app/models/projectsDto';
+import { Session } from 'src/app/services/session';
+import { Utils } from 'src/app/services/utils';
 import projectsData from '../../data/projects.json';
 
 @Component({
@@ -9,4 +12,21 @@ import projectsData from '../../data/projects.json';
 })
 export class ProjectComponent {
   data: ProjectsDto = projectsData;
+  item?: ProjectItemDto;
+  id?: number;
+
+  constructor(
+    public utils: Utils,
+    public session: Session,
+    public route: ActivatedRoute
+  ) {
+    this.session.showTopMenu=false;
+  }
+
+  ngOnInit(){
+    this.route.paramMap.subscribe( paramMap => {
+      this.id = parseInt(paramMap.get('id') ?? '0');
+      this.item = this.data.items.find(p => p.id == this.id);
+    });
+  }
 }
