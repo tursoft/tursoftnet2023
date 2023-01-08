2622d1d08e63faaf7b59fb89d83c8c771fa002fb
Muhammet Turşak <tursoft@gmail.com>
2023-01-08 22:13:12 +0300

    statistics page implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Session } from './services/session';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Muhammet Turşak';
+
+  constructor(public session: Session) {
+    
+  }
 }
