3e1a2d0acf9ddccc1ab24baba163805822e9ee6e
Muhammet Turşak <tursoft@gmail.com>
2023-01-24 00:10:03 +0300

    skills section converted to grid

--- src/app/pages/home/sections/skills/skills.component.ts
+++ src/app/pages/home/sections/skills/skills.component.ts
@@ -1,15 +1,7 @@
 import { Component } from '@angular/core';
-import { IHoverable } from '../../../../models/shared';
 import { SkillGroupDto, SkillDto, SkillsDto } from '../../../../models/skillsDto';
 import { Utils } from '../../../../modules/shared.module';
 import { SkillsRepository } from '../../../../services/repositories/skillsRepository';
-
-interface FilterDto {
-  filterText?: string,
-  group?: string
-}
-
-type SkillItem = SkillDto & IHoverable;
 
 @Component({
   selector: 'app-skills',
@@ -18,11 +10,8 @@ type SkillItem = SkillDto & IHoverable;
 })
 export class SkillsComponent {
   data: SkillsDto;
-  
   groups: SkillGroupDto[] = [];
-  allItems: SkillItem[] = [];
-  filteredItems: SkillItem[] = [];
-  filter: FilterDto = {};
+  items: SkillDto[] = [];
 
   constructor(
     public repo: SkillsRepository,
@@ -39,31 +28,6 @@ export class SkillsComponent {
     this.groups = [groupAll, ...this.data.general.groups];
 
     // prepare items
-    const allItems = this.repo.getItems().map(i => <SkillItem>i);
-    this.allItems = allItems;
-    
-    this.filterItems();
-  }
-
-  filterItems = () => {
-    const { filter: { filterText, group: filterGroup }, allItems} = this;
-    var filteredItems = this.utils.filterItemsByText(allItems, ['name', 'title', 'group'], filterText);
-    
-    if (!this.utils.isEmptyString(filterGroup)) {
-      filteredItems = filteredItems.filter(i => i.group == filterGroup);
-    }
-
-    this.filteredItems = filteredItems;
-  }
-
-  setGroup(group: string | undefined) {
-    this.filter.group = group;
-    this.filterItems();
-  }
-
-  clearFilter() {
-    this.filter.filterText = undefined;
-    this.filter.group = undefined;
-    this.filterItems();
+    this.items = this.repo.getItems();
   }
 }
